4fa4f7b694856e196dee8df588ff2751e497e1b5
JoeWood <joe.wood@gousto.co.uk>
2021-07-02 12:01:39 +0100

    Add bg colour change depending on the day of the week.

--- src/components/CanIGo.vue
+++ src/components/CanIGo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="flex flex-grow items-center justify-center h-screen bg-green">
+  <div id="app" class="flex flex-grow items-center justify-center h-screen" :class="backgroundColour">
 
 <div id="CanIGo" class="text-center md:w-3/4">
   <div class="uppercase h-48">
@@ -7,7 +7,7 @@
     <h2 class="text-6xl md:text-8xl">{{randomQuote}}</h2>
   </div>
         <button class="rounded mt-48 p-2 text-black border-2 border-solid text-opacity-70 focus:outline-none" @click="refreshQuote">Press me to update</button>
-        <footer class="text-uderline bg-green text-center flex-grow items-center">
+        <footer class="text-uderline text-center flex-grow items-center" :class="backgroundColour">
     <button class="p-4 underline background-transparent outline-none focus:outline-none" type="button" style="transition: all .15s ease">
   <a href="https://woodj22.github.io/#/">Made by Me </a>
 </button>
@@ -30,22 +30,23 @@ export default {
           randomQuote: "No",
           currentRandomNumber: null,
           backgroundColour: "bg-green",
-          endDate: new Date(2021, 4, 12, 0, 0),
-          timeToDisplay: "Month Days Hours Minutes Seconds",
           weeklyQuotes: {
-            "yes" : {"colour": "bg-green", "quotes" : ["Hell yes", "I am gagging for a pint!", "Pub. Now."]},
-            "no" : {"colour": "bg-red", "quotes" : ["hell no", "its a no from me.", "Another day, not today."]},
-            "maybe" : {"colour": "bg-amber", "quotes" :["I could be persuded", "Hmmm im not sure. Maybe?"]}
+            "yes" : {"colour": "bg-green", "quotes" : ["😍🍺","Always and forever.", "🍺👍🍺👍🍺👍🍺👍🍺👍🍺 ","🍻YURRRP🍻", "Hell yes.", "Which pub? That is the real question.", "I am gagging for a pint!", "Pub. Now.", "lets get them in.", "Who is buying the first round?", "I could do with a cold one. YES!", "yes🍺yes🍺yes"]},
+            "no" : {"colour": "bg-red", "quotes" : ["❌❌❌🍺❌❌❌","hell no", "its a no from me.", "Another day, just not today.", "Had a rough weekend. Taking it easy."]},
+            "maybe" : {"colour": "bg-amber", "quotes" :["I could be persuaded", "Hmmm im not sure. Maybe?", "its been a long week...", "oh I dont know."]}
           }
       }
   },
   methods: {
     refreshQuote(){
       const dayOfWeek = new Date().getDay()
+      // By default I have set it to be answering yes.
       let weeklyQuoteSelection = "yes"
+      // 4 is a wednesday 
       if(dayOfWeek == 4) {
         weeklyQuoteSelection = "maybe"
       }
+      // 3 is a tuesday. Anything before 3 will be answering no.
       if (dayOfWeek <= 3) {
         weeklyQuoteSelection = "no"
       }
